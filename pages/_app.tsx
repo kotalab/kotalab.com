24f7dca49d74b9ef5f00b9f98de568a6e17cb28f
kotalab <kotarolab@gmail.com>
2021-01-02 23:57:26 +0900

    Add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/global.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { AppProps } from 'next/app'
+import '../styles/global.css'
+import { pageview } from '../lib/gtag'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (isProduction) pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
